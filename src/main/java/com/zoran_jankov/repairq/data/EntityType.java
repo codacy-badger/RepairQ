a7d8436bad164762e399e4903cc6915a7a00816f
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-03-28 18:23:25 +0100

    Create EntityType.java

---
+++ src/main/java/com/zoran_jankov/repairq/data/EntityType.java
@@ -0,0 +1,74 @@
+package com.zoran_jankov.repairq.data;
+
+public enum EntityType
+{
+	NOTIFICATION,
+	NOTIFICATION_TYPE,
+	SERVICE,
+	SERVICE_TYPE,
+	TICKET,
+	STATUS,
+	CUSTOMER,
+	MARKETING,
+	LEGAL_ENTITY,
+	DEVICE,
+	MODEL,
+	DEVICE_TYPE,
+	BRAND,
+	USER;
+	
+	public static boolean hasDisplayName(EntityType type)
+	{
+		switch (type)
+		{
+        	case NOTIFICATION:
+        	case SERVICE:
+        	case TICKET:
+        	case DEVICE:
+        		return false;
+        		
+        	default:
+        		return true;
+		}
+	}
+	
+	public static boolean hasDisplayName(Entity entity)
+	{
+		return hasDisplayName(entity.getType());
+	}
+	
+	public static boolean makesReferences(Entity entity)
+	{
+		EntityType entityType = entity.getType();
+		
+		switch (entityType)
+		{
+        	case NOTIFICATION:
+        	case SERVICE:
+        	case TICKET:
+        	case CUSTOMER:
+        	case DEVICE:
+        	case MODEL:
+        		return true;
+        		
+        	default:
+        		return false;
+		}
+	}
+	
+	public static boolean isAProperty(EntityType entityType)
+	{
+		switch (entityType)
+		{
+        	case NOTIFICATION_TYPE:
+        	case MARKETING:
+        	case STATUS:
+        	case DEVICE_TYPE:
+        	case BRAND:
+        		return true;
+        		
+        	default:
+        		return false;
+		}
+	}
+}
